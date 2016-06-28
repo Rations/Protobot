b476a48dd4570400d025fc58931c59c592d11a0f
Rodion Raskolnikov <r4tions@gmail.com>
2016-06-28 10:27:22 -0500

    Revision

--- Protobot.ino
+++ Protobot.ino
@@ -1,41 +1,15 @@
-/*
- * Default (i.e., original) PS2 controls:
- * Right Joystick L/R: Gripper tip X position (side to side)
- * Right Joystick U/D: Gripper tip Y position (distance out from base center)
- * R1/R2 Buttons:      Gripper tip Z position (height from surface)
- * Left  Joystick L/R: Wrist rotate (if installed)
- * Left  Joystick U/D: Wrist angle
- * L1/L2 Buttons:      Gripper close/open
- * X Button:           Gripper fully open
- * Digital Pad U/D:    Speed increase/decrease
- */
 #include <Servo.h>
 #include <PS2X_lib.h>
 
 int dummy;                  // Defining this dummy variable to work around a bug in the IDE (1.0.3) pre-processor that messes up #ifdefs
 //#define DEBUG             // Uncomment to turn on debugging output
                             
-// Arduino pin numbers for servo connections
+// Specify Arduino pins for servos and PS2 controller connections. 
 #define ARM_SERVO_PIN 4     // Elbow Servo HS-755HB
-
-// Arduino pin numbers for PS2 controller connections
 #define PS2_CLK_PIN 9       // Clock
 #define PS2_CMD_PIN 7       // Command
 #define PS2_ATT_PIN 8       // Attention
 #define PS2_DAT_PIN 6       // Data
-
-// Define generic range limits for servos, in microseconds (us) and degrees (deg)
-// Used to map range of 180 deg to 1800 us (native servo units).
-#define SERVO_MIN_US 600
-#define SERVO_MID_US 1500
-#define SERVO_MAX_US 2400
-#define SERVO_MIN_DEG 0.0
-#define SERVO_MID_DEG 90.0
-#define SERVO_MAX_DEG 180.0
-
-// Speed adjustment parameters
-// Percentages (1.0 = 100%) - applied to all arm movements
-#define SPEED_DEFAULT 1.0
 
 // Practical navigation limit.
 // Enforced on controller input, and used for CLV calculation 
@@ -59,21 +33,15 @@ int dummy;                  // Defining this dummy variable to work around a bug
 #define PARK_READY 2        // Arm at Ready-To-Run position
 
 // Ready-To-Run arm position. See descriptions below
-// NOTE: Have the arm near this position before turning on the 
-//       servo power to prevent whiplash
-
- #define READY_X 0.0
-
+#define READY_X 0.0
 #define READY_Y 170.0
 #define READY_Z 45.0
-#define READY_G GRI_MID
 
 // Global variables for arm position, and initial settings
-
 float X = READY_X;         // Left/right distance (mm) from base centerline - 0 is straight
 float Y = READY_Y;          // Distance (mm) out from base center
 float Z = READY_Z;          // Height (mm) from surface (i.e. X/Y plane)
-float Speed = SPEED_DEFAULT;
+float Speed = 1.0;
 
 
 // Declare PS2 controller and servo objects
@@ -85,37 +53,36 @@ void setup() {
   Serial.begin(115200);
 #endif
   // Attach to the servos and specify range limits
-  arm_servo.attach(ARM_SERVO_PIN, SERVO_MIN_US, SERVO_MAX_US);
+  arm_servo.attach(ARM_SERVO_PIN);
   
   // Setup PS2 controller. Loop until ready.
   byte ps2_stat;
   do {
     ps2_stat = Ps2x.config_gamepad(PS2_CLK_PIN, PS2_CMD_PIN, PS2_ATT_PIN, PS2_DAT_PIN);
-#ifdef DEBUG
-        if (ps2_stat == 1)
-            Serial.println("No controller found. Re-trying ...");
-#endif
+                              #ifdef DEBUG
+                                      if (ps2_stat == 1)
+                                          Serial.println("No controller found. Re-trying ...");
+                              #endif
   } while (ps2_stat == 1);
  
-#ifdef DEBUG
-  switch (ps2_stat) {
-    case 0:
-      Serial.println("Found Controller, configured successfully.");
-      break;
-    case 2:
-      Serial.println("Controller found but not accepting commands.");
-      break;
-    case 3:
-      Serial.println("Controller refusing to enter 'Pressures' mode, may not support it. ");      
-      break;
-  }
-#endif
+                              #ifdef DEBUG
+                                switch (ps2_stat) {
+                                  case 0:
+                                    Serial.println("Found Controller, configured successfully.");
+                                    break;
+                                  case 2:
+                                    Serial.println("Controller found but not accepting commands.");
+                                    break;
+                                  case 3:
+                                    Serial.println("Controller refusing to enter 'Pressures' mode, may not support it. ");      
+                                    break;
+                                }
+                              #endif
     servo_park(PARK_READY);
 
-#ifdef DEBUG
-    Serial.println("Start");
-#endif
-
+                              #ifdef DEBUG
+                                  Serial.println("Start");
+                              #endif
     delay(500);
 }
  
@@ -188,10 +155,10 @@ int set_arm(float x, float y, float z)
     
     // Position the servos
     arm_servo.writeMicroseconds(deg_to_us(elb_pos));
-#ifdef DEBUG
-  //DEBUGGING PRINTING GOES HERE
-  Serial.println();
-#endif
+                                                                                                                                      #ifdef DEBUG
+                                                                                                                                        //DEBUGGING PRINTING GOES HERE
+                                                                                                                                        Serial.println();
+                                                                                                                                      #endif
     return IK_SUCCESS;
 }
  
@@ -213,15 +180,6 @@ void servo_park(int park_type) {
 
 // Converts deg to us to take advantage of additional servo resolution.
 int deg_to_us(float value) {
-    // Apply basic constraints
-    if (value < SERVO_MIN_DEG) value = SERVO_MIN_DEG;
-    if (value > SERVO_MAX_DEG) value = SERVO_MAX_DEG;
-    
     // Map degrees to microseconds, and round the result to a whole number
-    return(round(map_float(value, SERVO_MIN_DEG, SERVO_MAX_DEG, (float)SERVO_MIN_US, (float)SERVO_MAX_US)));      
-}
-
-// Same logic as native map() function, just operates on float instead of long
-float map_float(float x, float in_min, float in_max, float out_min, float out_max) {
-  return ((x - in_min) * (out_max - out_min) / (in_max - in_min)) + out_min;
+    return(round(((value - SERVO_MIN_DEG) * ((float)SERVO_MAX_US - (float)SERVO_MIN_US) / (SERVO_MAX_DEG - SERVO_MIN_DEG)) + (float)SERVO_MIN_US));      
 }
